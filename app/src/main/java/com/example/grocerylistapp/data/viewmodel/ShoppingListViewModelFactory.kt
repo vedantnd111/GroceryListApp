3cc11748515121f87c30ec28069d276b8cc79312
Vedant Deshpande <vedantnd111@gmail.com>
2023-06-27 09:41:34 +0530

    removed some warnings

--- app/src/main/java/com/example/grocerylistapp/data/viewmodel/ShoppingListViewModelFactory.kt
+++ app/src/main/java/com/example/grocerylistapp/data/viewmodel/ShoppingListViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.grocerylistapp.data.repository.ShoppingListRepository
 
+@SuppressWarnings("Unchecked")
 class ShoppingListViewModelFactory(private val repository: ShoppingListRepository):ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
